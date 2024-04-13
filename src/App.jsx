0b5a2bf494f43e318d622307dc396cbbb3986e50
ElynaRia <elynaria28@gmail.com>
2024-04-13 13:20:24 +0700

    not found

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './Pages/Home'
 import Pcsx2_16 from './Pages/Pcsx2_16'
 import Pcsx2_17 from './Pages/Pcsx2_17'
+import NotFound from './Pages/NotFound'
 
 export default function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="*" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
         <Route path="/pcsx2-stable" element={<Pcsx2_16 />} />
         <Route path="/pcsx2-nightly" element={<Pcsx2_17 />} />
       </Routes >
--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -3,6 +3,9 @@ import React from 'react'
 export default function NotFound() {
     return (
         <>
+            <section className='fixed bg-black inset-x-0 inset-y-0 w-full h-full flex justify-center items-center'>
+                <h1 className='text-2xl uppercase'>NOT FOUND</h1>
+            </section>
         </>
     )
 }
